fb0489a43bad352737deb9b5de8146ad9fb3aa8a
GBRBD <gaborattilabodi@gmail.com>
2020-01-14 21:49:57 +0100

    Managing Data & Images via the Provider Package

--- lib/screens/places_list.screen.dart
+++ lib/screens/places_list.screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './add_place.screen.dart';
+import '../providers/great_places.provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -11,17 +13,33 @@ class PlacesListScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () => onPlacesAdd(context),
-          )
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child: const Text('Got no places yet, start adding some!'),
+        ),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+            ? ch
+            : ListView.builder(
+                itemCount: greatPlaces.items.length,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(
+                      greatPlaces.items[i].image,
+                    ),
+                  ),
+                  title: Text(greatPlaces.items[i].title),
+                  onTap: () {
+                    // Go to detail page ...
+                  },
+                ),
+              ),
       ),
     );
-  }
-
-  void onPlacesAdd(BuildContext context) {
-    Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
   }
 }
